0848323850098f8e46dee1fe21a4c3b63abd1ff4
leedthanh <leedthanh@gmail.com>
2023-06-30 17:15:17 -0500

    update

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,17 +1,27 @@
-import pandas as pd
+
+import csv
 from pathlib import Path
 
 csv_path = Path('/Users/thanhle/GitHub/python-challenge/PyPoll/Resources/election_data.csv')
+candidate_list = []
+candidate_vote = {}
+with open(csv_path, encoding='UTF-8') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
 
-poll_df = pd.read_csv(csv_path)
+    header = next(csvreader)
+    total_vote = 0
+    for row in csvreader:
+        total_vote  +=1
+        if row [2] not in  candidate_list:
+            candidate_list.append(row[2])
+            candidate_vote[row[2]] = 0
+        candidate_vote[row[2]] += 1
 
-poll_df.head()
-print("Election Resullts")
-print("----------------------------------")
+    for candidate in candidate_vote:
+        percent_vote = candidate_vote[candidate] / total_vote * 100
 
-total_vote_df = poll_df["Ballot ID"].nunique()
+        print(f'{candidate}: {percent_vote}% {candidate_vote[candidate]}')
 
-print("Total Votes:",total_vote_df)
-print("----------------------------------")
-
-
+print(candidate_vote)
+print(candidate_list)
+print(total_vote)
