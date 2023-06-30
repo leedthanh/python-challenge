e4049e5d4de639d594e691ac582ed2651c744ad5
leedthanh <leedthanh@gmail.com>
2023-06-30 00:54:22 -0500

    pypoll starting got the total votes

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -6,4 +6,12 @@ csv_path = Path('/Users/thanhle/GitHub/python-challenge/PyPoll/Resources/electio
 poll_df = pd.read_csv(csv_path)
 
 poll_df.head()
+print("Election Resullts")
+print("----------------------------------")
+
+total_vote_df = poll_df["Ballot ID"].nunique()
+
+print("Total Votes:",total_vote_df)
+print("----------------------------------")
+
 
